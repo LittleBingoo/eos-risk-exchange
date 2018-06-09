4e9b96562efbdbcacd79e61a6ecaf445277872d1
forrest <zyyu1990@gmail.com>
2018-06-09 22:09:09 +0800

    js

---
+++ exchange/js/risk.js
@@ -0,0 +1,293 @@
+var chain_id = 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
+var httpEndpoint = 'http://47.97.76.2:8889'
+
+/**
+ *  ERROR
+ * */
+function reject(err,callback) {
+    callback({status:false,message:err})
+}
+/**
+ * EOS API
+ * */
+function getEOS(privateKey) {
+    // EOS configuration..
+    var config = {
+        chainId: chain_id,
+        httpEndpoint: httpEndpoint,
+        keyProvider:privateKey,
+        expireInSeconds: 60
+    }
+    return Eos(config)
+}
+function eos_randomAccount(creator,name,creator_privateKey,callback){
+    setTimeout(function () {
+        // keys
+        var _eos_ecc = eos_ecc,
+            PrivateKey = _eos_ecc.PrivateKey
+        var d = PrivateKey.randomKey()
+        var privateKey = d.toWif()
+        var publicKey = d.toPublic().toString()
+
+        // New Account
+        var eos = getEOS(creator_privateKey)
+        eos.transaction(tr => {
+            tr.newaccount({
+            creator: creator,
+            name: name,
+            owner: publicKey,
+            active: publicKey
+            }),
+            tr.buyrambytes({
+                payer: creator,
+                receiver: name,
+                bytes: 8192
+            }),
+            tr.delegatebw({
+                from: creator,
+                receiver: name,
+                stake_net_quantity: '0.0001 EOS',
+                stake_cpu_quantity: '0.0001 EOS',
+                transfer: 0
+            })
+        }).then(result => {
+            callback({status:true,message:{'name':name,'publicKey':publicKey,'privateKey':privateKey}})
+        }).catch(err => {
+            reject(err,callback)
+        })
+
+
+    }, 100)
+}
+function eos_balance(account,callback){
+    var eos = getEOS(null)
+    eos.getCurrencyBalance("eosio.token",account,"EOS").then(result => {
+        callback({status:true,message:result})
+    }).catch(err => {
+        reject(err,callback)
+    })
+
+}
+function eos_transfer(from,to,quantity,memo,from_privateKey,callback){
+    var eos = getEOS(from_privateKey)
+    // Transfer
+    eos.transfer(from,to, quantity, memo,true).then(res => {
+        callback({status:true,message:res})
+    }).catch(err => {
+        reject(err,callback)
+    })
+}
+
+
+/**
+ *  House Rental
+ *  @param renter 加入账户
+ *  @param address 住址
+ *  @param content 风险类型
+ *  @param renter_privateKey 加入账户的owner私钥
+ *  @param callback callback
+ * */
+function house_rental_join(renter,address,content,renter_privateKey,callback) {
+    // keys
+    var _eos_ecc = eos_ecc,
+        PrivateKey = _eos_ecc.PrivateKey
+    var renterPublicKey = PrivateKey.fromWif(renter_privateKey).toPublic().toString()
+    // returns Promise
+    var eos = getEOS(renter_privateKey)
+    var auth = {
+        "threshold": 1,
+        "keys": [
+            {"key": renterPublicKey,"weight": 1}
+            ],
+        "accounts": [
+            {"permission":{"actor":"mutualaideos","permission":"eosio.code"},"weight":1}]
+    }
+    eos.transaction(tr => {
+        tr.updateauth({
+            account: renter,
+            permission: 'active',
+            parent: 'owner',
+            auth: auth
+        })
+    }).then(result => {
+        eos.transaction({
+            actions: [
+            {
+                account: 'mutualaideos',
+                name: 'join',
+                authorization: [{
+                    actor: renter,
+                    permission: 'active'
+                }],
+                data: {
+                    renter: renter,
+                    address: address,
+                    content: content,
+                    quantity: '1.0000 EOS'
+                }
+            }
+            ]
+         }).then(res => {
+            callback({status:true,message:res})
+         }).catch(err => {
+            reject(err,callback)
+         })
+    }).catch(err => {
+        reject(err,callback)
+    })
+    
+}
+
+/**
+ *  House Rental - Joined
+ *  @param callback callback
+ * */
+function house_rental_joined(callback) {
+    var eos = getEOS(null)
+    eos.getTableRows({
+        scope: 'mutualaideos',
+        code: 'mutualaideos',
+        table: 'participator',
+        json: true
+    }).then(res => {
+        callback({status:true,message:res})
+    }).catch(err => {
+        reject(err,callback)
+    })
+}
+
+/**
+ *  House Rental - sale
+ *  @param asker    卖方账号名
+ *  @param number   卖出risktoken数量（必须整数）
+ *  @param price    多少个EOS
+ *  @param callback callback
+ * */
+function house_rental_rnt_sale(asker,number,price,asker_privateKey,callback) {
+    var eos = getEOS(asker_privateKey)
+    eos.transaction({
+        actions: [
+            {
+                account: 'mutualaideos',
+                name: 'make',
+                authorization: [{
+                    actor: asker,
+                    permission: 'active'
+                }],
+                data: {
+                    asker: asker,
+                    number: number,
+                    price: price
+                }
+            }
+        ]
+    }).then(res => {
+        callback({status:true,message:res})
+    }).catch(err => {
+        reject(err,callback)
+    })
+}
+/**
+ *  House Rental - buy
+ *  @param buyer    买入方账号名
+ *  @param from     卖出方账号名
+ *  @param number   买入数量
+ *  @param callback callback
+ * */
+function house_rental_rnt_buy(buyer,from,number,buyer_privateKey,callback) {
+    var eos = getEOS(buyer_privateKey)
+    eos.transaction({
+        actions: [
+            {
+                account: 'mutualaideos',
+                name: 'take',
+                authorization: [{
+                    actor: buyer,
+                    permission: 'active'
+                }],
+                data: {
+                    taker: buyer,
+                    from: from,
+                    number: number
+                }
+            }
+        ]
+    }).then(res => {
+        callback({status:true,message:res})
+    }).catch(err => {
+        reject(err,callback)
+    })
+}
+/**
+ *  House Rental - claim
+ *  @param holder    RNT持有者
+ *  @param callback callback
+ * */
+function house_rental_rnt_claim(holder,holder_privateKey,callback) {
+    var eos = getEOS(holder_privateKey)
+    eos.transaction({
+        actions: [
+            {
+                account: 'mutualaideos',
+                name: 'claim',
+                authorization: [{
+                    actor: holder,
+                    permission: 'active'
+                }],
+                data: {
+                    user: holder
+                }
+            }
+        ]
+    }).then(res => {
+        callback({status:true,message:res})
+    }).catch(err => {
+        reject(err,callback)
+    })
+}
+/**
+ * House Rental - validate
+ *  @param verifier 验证管理员
+ *  @param callback callback
+ * */
+function house_retal_events(callback) {
+    var eos = getEOS(null)
+    eos.getTableRows({
+        scope: 'validator',
+        code: 'validator',
+        table: 'event',
+        json: true
+    }).then(res => {
+        callback({status:true,message:res})
+    }).catch(err => {
+        reject(err,callback)
+    })
+}
+/**
+ *  House Rental - validate
+ *  @param verifier 验证管理员
+ *  @param callback callback
+ * */
+function house_rental_validate(verifier,event_id,verifier_privateKey,callback) {
+    var eos = getEOS(verifier_privateKey)
+    eos.transaction({
+        actions: [
+            {
+                account: 'validator',
+                name: 'validate',
+                authorization: [{
+                    actor: verifier,
+                    permission: 'active'
+                }],
+                data: {
+                    verifier: verifier,
+                    event_id: event_id
+                }
+            }
+        ]
+    }).then(res => {
+        callback({status:true,message:res})
+    }).catch(err => {
+        reject(err,callback)
+    })
+}
